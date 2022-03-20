e03ce75f0c7e4e1a1f76ca543ee6aeb700a2190d
Banh Ngoc Nhu Y <83709137+reallnhwy@users.noreply.github.com>
2022-03-20 23:58:20 +0700

    Additional Features update

--- YBanhsFlashcardApp/app/src/main/java/com/example/ybanhsflashcardapp/AddCardActivity.java
+++ YBanhsFlashcardApp/app/src/main/java/com/example/ybanhsflashcardapp/AddCardActivity.java
@@ -15,8 +15,8 @@ public class AddCardActivity extends AppCompatActivity {
 
     public EditText question_box;
     public EditText answer_box;
-//    public EditText wronganswer1_box;
-//    public EditText wronganswer2_box;
+    public EditText wronganswer1_box;
+    public EditText wronganswer2_box;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +28,15 @@ public class AddCardActivity extends AppCompatActivity {
 
         question_box = findViewById(R.id.question_box);
         answer_box = findViewById(R.id.answer_box);
-//        wronganswer1_box = findViewById(R.id.wronganswer1_box);
-//        wronganswer2_box = findViewById(R.id.wronganswer2_box);
+        wronganswer1_box = findViewById(R.id.wronganswer1_box);
+        wronganswer2_box = findViewById(R.id.wronganswer2_box);
 
+        String currentQuestion = getIntent().getStringExtra("flashcard_question");
+        String currentCorrectAnswer = getIntent().getStringExtra("flashcard_answer");
+        String currentWrongAnswer1 = getIntent().getStringExtra("wronganswer1");
+        String currentWrongAnswer2 = getIntent().getStringExtra("wronganswer2");
 
-
+        //This fucntion is to cancel editing or adding a flashcard (destroy the AddCardActivity)
         cancel_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,37 +44,36 @@ public class AddCardActivity extends AppCompatActivity {
             }
         });
 
+        //This function is to send the data from EditText to onActivityResult function
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Get data from EditText
                 String question = question_box.getText().toString();
                 String answer = answer_box.getText().toString();
-//                String wronganswer1 = wronganswer1_box.getText().toString();
-//                String wronganswer2 = wronganswer2_box.getText().toString();
+                String wronganswer1 = wronganswer1_box.getText().toString();
+                String wronganswer2 = wronganswer2_box.getText().toString();
 
-                // Put data in the intent
-                Intent intent = new Intent();
-                intent.putExtra( "QUESTION_KEY",question);
-                intent.putExtra( "ANSWER_KEY",answer);
-//                intent.putExtra("WRONG_ANSWER1",wronganswer1);
-//                intent.putExtra("WRONG_ANSWER2",wronganswer2);
-                setResult(RESULT_OK,intent);
                 // Must use equals() method
-                if (question.equals("") || answer.equals("")) {
+                if (question.equals("") || answer.equals("") || wronganswer2.equals("") || wronganswer1.equals("")) {
                     Toast.makeText(getApplicationContext(), "Must enter both Question and Answer?", Toast.LENGTH_SHORT).show();
                 } else {
+                    // Put data in the intent
+                    Intent intent = new Intent();
+                    intent.putExtra( "QUESTION_KEY",question);
+                    intent.putExtra( "ANSWER_KEY",answer);
+                    intent.putExtra("WRONG_ANSWER1",wronganswer1);
+                    intent.putExtra("WRONG_ANSWER2",wronganswer2);
+                    setResult(RESULT_OK,intent);
                     finish();
                 }
             }
         });
 
-        String currentQuestion = getIntent().getStringExtra("flashcard_question");
-        String currentAnswer = getIntent().getStringExtra("flashcard_answer");
         question_box.setText(currentQuestion);
-        answer_box.setText(currentAnswer);
-
-
+        answer_box.setText(currentCorrectAnswer);
+        wronganswer1_box.setText(currentWrongAnswer1);
+        wronganswer2_box.setText(currentWrongAnswer2);
 
     }
 }
--- YBanhsFlashcardApp/app/src/main/java/com/example/ybanhsflashcardapp/MainActivity.java
+++ YBanhsFlashcardApp/app/src/main/java/com/example/ybanhsflashcardapp/MainActivity.java
@@ -29,65 +29,121 @@ public class MainActivity extends AppCompatActivity {
 
         flashcard_question = findViewById(R.id.flashcard_question);
         flashcard_answer = findViewById(R.id.flashcard_answer);
-//        wronganswer1 = findViewById(R.id.answer1);
-//        wronganswer2 = findViewById(R.id.answer2);
+        ImageView ToggleAnswer = findViewById(R.id.toggle_choices_visibility);
+        wronganswer1 = findViewById(R.id.answer1);
+        wronganswer2 = findViewById(R.id.answer2);
+        final boolean[] IsShowingAnswer = {true};
+        final boolean[] IsAnswerClicked = {false};
 
 
-        // Tap the question to see the flashcard answer
-        flashcard_question.setOnClickListener(new View.OnClickListener() {
+        // Function to hide and show answer
+        ToggleAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Show the answer and hide the question
-                flashcard_question.setVisibility(View.INVISIBLE);
-                flashcard_answer.setVisibility(View.VISIBLE);
-//                findViewById(R.id.parent).setBackgroundColor(getResources().getColor(R.color.beige));
+                if (IsShowingAnswer[0]) {
+                    ToggleAnswer.setImageResource(R.drawable.ic_eye_crossed);
+                    wronganswer1.setVisibility(View.INVISIBLE);
+                    wronganswer2.setVisibility(View.INVISIBLE);
+                    flashcard_answer.setVisibility(View.INVISIBLE);
+                    IsShowingAnswer[0] = false;
+                } else {
+                    ToggleAnswer.setImageResource(R.drawable.ic_eye);
+                    wronganswer1.setVisibility(View.VISIBLE);
+                    wronganswer2.setVisibility(View.VISIBLE);
+                    flashcard_answer.setVisibility(View.VISIBLE);
+                    IsShowingAnswer[0] = true;
+                }
             }
         });
 
-        // Tap to toggle the view
-        flashcard_answer.setOnClickListener(new View.OnClickListener() {
+        // Reset answer
+        findViewById(R.id.parent).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Show the question again and hide the answer
-                flashcard_question.setVisibility(View.VISIBLE);
-                flashcard_answer.setVisibility(View.INVISIBLE);
-//                findViewById(R.id.parent).setBackgroundColor(getResources().getColor(R.color.darkred));
+                wronganswer1.setTextColor(getResources().getColor(R.color.darkblue));
+                wronganswer2.setTextColor(getResources().getColor(R.color.darkblue));
+                flashcard_answer.setTextColor(getResources().getColor(R.color.darkblue));
+                IsAnswerClicked[0] = false;
             }
         });
 
+
+        // Function to add a new flashcard (start AddCardActivity class and expect some data in return)
         findViewById(R.id.add_icon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-//                MainActivity.this.startActivity(intent);
-                startActivityForResult(intent, 1);
+//                MainActivity.this.startActivity(intent); //This is to only start the activity
+                startActivityForResult(intent, 100);
             }
         });
 
+        // Function to edit an already exist flashcard (start AddCardActivity class and expect some data in return)
         findViewById(R.id.edit_icon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 intent.putExtra("flashcard_question", flashcard_question.getText().toString());
-                intent.putExtra("flashcard_answer",flashcard_answer.getText().toString());
-                startActivityForResult(intent,1);
+                intent.putExtra("flashcard_answer", flashcard_answer.getText().toString());
+                intent.putExtra("wronganswer1", wronganswer1.getText().toString());
+                intent.putExtra("wronganswer2", wronganswer2.getText().toString());
+                startActivityForResult(intent, 100);
+            }
+        });
+
+
+        //The next 3 Listeners may turn into a function?
+        flashcard_answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!IsAnswerClicked[0]) {
+                    flashcard_answer.setTextColor(getResources().getColor(R.color.green));
+                    IsAnswerClicked[0] = true;
+                }
+            }
+        });
+
+        wronganswer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!IsAnswerClicked[0]) {
+                    wronganswer2.setTextColor(getResources().getColor(R.color.darkred));
+                    flashcard_answer.setTextColor(getResources().getColor(R.color.green));
+                    IsAnswerClicked[0] = true;
+                }
+            }
+        });
+
+        wronganswer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!IsAnswerClicked[0]) {
+                    wronganswer1.setTextColor(getResources().getColor(R.color.darkred));
+                    flashcard_answer.setTextColor(getResources().getColor(R.color.green));
+                    IsAnswerClicked[0] = true;
+                }
             }
         });
 
     }
 
+    // This function is to receive data and do something with it
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == 1) {
+        if (resultCode == RESULT_OK && requestCode == 100) {
             if (data != null) {
+                //Get the data and convert it to String
                 String question = data.getExtras().getString("QUESTION_KEY");
                 String answer = data.getExtras().getString("ANSWER_KEY");
-//                String wronganswer1= data.getExtras().getString("WRONG_ANSWER1");
-//                String wronganswer2= data.getExtras().getString("WRONG_ANSWER1");
+                String WrongAnswer1 = data.getExtras().getString("WRONG_ANSWER1");
+                String WrongAnswer2 = data.getExtras().getString("WRONG_ANSWER2");
 
+                //Change the TextView with the received Strings
                 flashcard_question.setText(question);
                 flashcard_answer.setText(answer);
+                wronganswer1.setText(WrongAnswer1);
+                wronganswer2.setText(WrongAnswer2);
 
             }
         }
@@ -98,3 +154,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+//        // Tap the question to see the flashcard answer
+//        flashcard_question.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Show the answer and hide the question
+//                flashcard_question.setVisibility(View.INVISIBLE);
+//                flashcard_answer.setVisibility(View.VISIBLE);
+////                findViewById(R.id.parent).setBackgroundColor(getResources().getColor(R.color.beige));
+//            }
+//        });
+//
+//        // Tap to toggle the view
+//        flashcard_answer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Show the question again and hide the answer
+//                flashcard_question.setVisibility(View.VISIBLE);
+//                flashcard_answer.setVisibility(View.INVISIBLE);
+////                findViewById(R.id.parent).setBackgroundColor(getResources().getColor(R.color.darkred));
+//            }
+//        });
